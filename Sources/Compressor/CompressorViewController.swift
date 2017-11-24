077fbc2a88afb12d10f298b1f1359fc7ec7bde6b
Merrick Sapsford <merrick@sapsford.tech>
2017-11-24 14:47:20 +0100

    Add basic layout components

--- Sources/Compressor/CompressorViewController.swift
+++ Sources/Compressor/CompressorViewController.swift
@@ -9,7 +9,47 @@
 import UIKit
 import Tabman
 import Pageboy
+import PureLayout
 
 open class CompressorViewController: TabmanViewController {
     
+    // MARK: Properties
+    
+    private var headerStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        return stackView
+    }()
+    private var headerContentView: UIView = {
+        let view = UIView()
+        // TODO - Remove
+        view.backgroundColor = .red
+        view.autoSetDimension(.height, toSize: 300.0)
+        return view
+    }()
+    private var barContentView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .blue
+        return view
+    }()
+    
+    // MARK: Lifecycle
+    
+    open override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // TODO - Do this properly
+        view.addSubview(headerStackView)
+        headerStackView.autoPinEdgesToSuperviewEdges(with: .zero, excludingEdge: .bottom)
+
+        embedBar(in: barContentView)
+        
+        headerStackView.addArrangedSubview(headerContentView)
+        headerStackView.addArrangedSubview(barContentView)
+        
+        // TODO - Fix this in Tabman
+        bar.appearance = TabmanBar.Appearance({ (appearance) in
+            appearance.layout.extendBackgroundEdgeInsets = false
+        })
+    }
 }
